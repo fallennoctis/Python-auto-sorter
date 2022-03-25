771018809afa2f5947162306064868c535f4f12d
fallennoctis <51355136+fallennoctis@users.noreply.github.com>
2022-03-24 22:47:30 -0500

    Full refactor
    Complete refactor, Cleaned up the million if statements, and change over to a dictionary to make life easier. Added try and excepts to make sure that the file being open doesn't kill the program.

--- Python auto sorter.py
+++ Python auto sorter.py
@@ -5,69 +5,70 @@ from typing import List
 
 User = getpass.getuser()
 path = ('C:/Users/' + User + '/Downloads/')
+subPath = (path + 'Documents/')
 names = os.listdir(path)
+subNames = os.listdir(subPath)
 folder_name: List[str] = ['Programs Installers', 'Books', 'Pics', 'Documents', 'AHK Scripts', 'RARs',
                           'Cheat tables', 'SC2 Banks and Replays']
-# for x in range(0, 8):
-for x in range(0, len(folder_name)):
-    if not os.path.exists(path + folder_name[x]):
-        os.makedirs(path + folder_name[x])
-
-subPath = (path + 'Documents/')
-subNames = os.listdir(subPath)
 folderSubNames: List[str] = ['Excels', 'Word', 'PDFs', 'Logs', 'Text Files', 'Power Points']
-for x in range(0, len(folderSubNames)):
-    if not os.path.exists(subPath + folderSubNames[x]):
-        os.makedirs(subPath + folderSubNames[x])
+#                fileExtension: 'locations'
+fileTypeDict = {"exe": 'Programs Installers/',
+                "msi": 'Programs Installers/',
+                "jar": 'Programs Installers/',
+                "epub": 'Books/',
+                "png": 'Pics/',
+                "jpg": 'Pics/',
+                "ahk": 'AHK Scripts/',
+                "rar": 'RARs/',
+                "zip": 'RARs/',
+                "7z": 'RARs/',
+                "ct": 'Cheat tables/',
+                "xlsx": 'Documents/Excels/',
+                "xlsm": 'Documents/Excels/',
+                "csv": 'Documents/Excels/',
+                "doc": 'Documents/Word/',
+                "pdf": 'Documents/PDFs/',
+                "log": 'Documents/Logs/',
+                "ppt": 'Documents/Power Points/',
+                "txt": 'Documents/Text Files/'
+                }
+
+for fol_nam in folder_name:
+    if not os.path.exists(path + fol_nam):
+        os.makedirs(path + fol_nam)
+
+for fol_nam in folderSubNames:
+    if not os.path.exists(subPath + fol_nam):
+        os.makedirs(subPath + fol_nam)
 
 for file_name in names:
-    files = file_name.lower()
-    if ".exe" in files or ".msi" in files or ".jar" in files and not \
-            os.path.exists(path + 'Programs Installers/' + files):
-        shutil.move(path + files, path + 'Programs Installers/' + files)
-
-    if ".epub" in files and not os.path.exists(path + 'Books/' + files):
-        shutil.move(path + files, path + 'Books/' + files)
-
-    if ".png" in files and not os.path.exists(path + 'Pics/' + files):
-        shutil.move(path + files, path + 'Pics/' + files)
-
-    if ".jpg" in files or ".JPG" in files and not os.path.exists(path + 'Pics/' + files):
-        shutil.move(path + files, path + 'Pics/' + files)
-
-    if ".ahk" in files and not os.path.exists(path + 'AHK Scripts/' + files):
-        shutil.move(path + files, path + 'AHK Scripts/' + files)
-
-    if ".rar" in files or ".zip" in files or ".7z" in files and not \
-            os.path.exists(path + 'RARs/' + files):
-        shutil.move(path + files, path + 'RARs/' + files)
-
-    if ".ct" in files and not os.path.exists(path + 'Cheat tables/' + files):
-        shutil.move(path + files, path + 'Cheat tables/' + files)
-
-    if ".SC2Bank" in files and not os.path.exists(path + 'SC2 Banks and Replays/' + files):
-        shutil.move(path + files, path + 'SC2 Banks and Replays/' + files)
-
-    # Breaking sub paths and paths apart
-
-    if ".xls" in files or ".csv" in files and not os.path.exists(subPath + 'Excels/' + files):
-        shutil.move(path + files, subPath + 'Excels/' + files)
-
-    if ".doc" in files and not os.path.exists(subPath + 'Word/' + files):
-        shutil.move(path + files, subPath + 'Word/' + files)
-
-    if ".pdf" in files and not os.path.exists(subPath + 'PDFs/' + files):
-        shutil.move(path + files, subPath + 'PDFs/' + files)
-
-    if ".log" in files and not os.path.exists(subPath + 'Logs/' + files):
-        shutil.move(path + files, subPath + 'Logs/' + files)
-
-    if ".ppt" in files and not os.path.exists(subPath + 'Power Points/' + files):
-        shutil.move(path + files, subPath + 'Power Points/' + files)
-
-    if ".txt" in files and not os.path.exists(subPath + 'Text Files/' + files):
-        shutil.move(path + files, subPath + 'Text Files/' + files)
-
+    files = file_name.lower().split(".")[-1]
+    x = 1
+    if files in fileTypeDict.keys():
+        locations = fileTypeDict.get(files)
+        if os.path.exists(path + locations + file_name): # file is in location
+            fileVar = file_name.split(".")
+            file_name2 = str(fileVar[0]) + "(" + str(x) + ")" + "." + str(files)
+            while True:
+                if os.path.exists(path + locations + file_name2):  # file is in location
+                    fileVar = file_name.split(".")
+                    x += 1
+                    file_name2 = str(fileVar[0]) + "(" + str(x) + ")" + "." + str(files)
+                else:
+                    try:
+                        shutil.move(path + file_name, path + locations + file_name2)
+                    except PermissionError as err:
+                        print(err)
+                    break
+        else:
+            try:
+                shutil.move(path + file_name, path + locations + file_name)
+            except PermissionError as err:
+                print(err)
+    elif file_name in folder_name or file_name in folderSubNames or file_name == "desktop.ini":
+        pass
+    else:
+        print(f"{file_name} Not found")
 
 for root, dirs, files in os.walk(path, topdown=False):
     for name in dirs:
@@ -76,7 +77,3 @@ for root, dirs, files in os.walk(path, topdown=False):
             # print("This is the command to delete for testing deletions :", os.path.join(root, name))
             if os.rmdir(os.path.join(root, name)):
                 print('Successfully deleted', os.path.join(root, name))
-
-
-
-

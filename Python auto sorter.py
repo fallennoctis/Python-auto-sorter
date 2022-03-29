3df113a1b908eabf53f20805b56a8d2098283aa2
fallennoctis <51355136+fallennoctis@users.noreply.github.com>
2022-03-29 15:20:28 -0500

    Added file comparison to squash a copy dupe bug and cleared up pythonic stuff

--- Python auto sorter.py
+++ Python auto sorter.py
@@ -1,9 +1,8 @@
+import configparser
+import getpass
 import os
 import shutil
-import getpass
-import configparser
-from typing import List
-# from tkinter import Tk
+import filecmp
 from tkinter.filedialog import askdirectory
 
 User = getpass.getuser()
@@ -11,10 +10,10 @@ path = ('C:/Users/' + User + '/Downloads/')
 subPath = (path + 'Documents/')
 names = os.listdir(path)
 subNames = os.listdir(subPath)
-folder_name: List[str] = ['Programs Installers', 'Books', 'Pics', 'Documents', 'AHK Scripts', 'RARs',
-                          'Cheat tables', 'SC2 Banks and Replays']
-folderSubNames: List[str] = ['Excels', 'Word', 'PDFs', 'Logs', 'Text Files', 'Power Points']
+folderName = ['Programs Installers', 'Books', 'Pics', 'Documents', 'AHK Scripts', 'RARs']
+folderSubNames = ['Excels', 'Word', 'PDFs', 'Logs', 'Text Files', 'Power Points']
 
+# Structure for the dictionary is as follows:
 #                fileExtension: 'locations'
 fileTypeDict = {"exe": 'Programs Installers/',
                 "msi": 'Programs Installers/',
@@ -31,20 +30,21 @@ fileTypeDict = {"exe": 'Programs Installers/',
                 "xlsm": 'Documents/Excels/',
                 "csv": 'Documents/Excels/',
                 "doc": 'Documents/Word/',
+                "docx": 'Documents/Word/',
                 "pdf": 'Documents/PDFs/',
                 "log": 'Documents/Logs/',
                 "ppt": 'Documents/Power Points/',
                 "txt": 'Documents/Text Files/'
                 }
 
-if os.path.exists(path) is False:
-    if os.path.exists("Auto_sorter_configfile.ini"):
+if os.path.exists(path) is False:  # Check the normal file path and if not allowing selection of it; set true to pick
+    if os.path.exists("Auto_sorter_configfile.ini"):  # checks for the ini file and loads it
         config_obj = configparser.ConfigParser()
         config_obj.read("Auto_sorter_configfile.ini")
         filLocation = config_obj['File Location']
         path = filLocation["path"]
         subPath = filLocation["subPath"]
-    else:
+    else:  # if needed creates the file for the config
         print(f'Unable to locate folder {path}')
         path = askdirectory(title='Select Your Downloads folder')
         path = str(path + "/")
@@ -59,7 +59,7 @@ if os.path.exists(path) is False:
         except PermissionError as err:
             print(err)
 
-for fol_nam in folder_name:
+for fol_nam in folderName:
     if not os.path.exists(path + fol_nam):
         os.makedirs(path + fol_nam)
 
@@ -73,33 +73,36 @@ for file_name in names:
     if files in fileTypeDict.keys():
         locations = fileTypeDict.get(files)
         if os.path.exists(path + locations + file_name):  # file is in location
-            fileVar = file_name.split(".")
-            file_name2 = str(fileVar[0]) + "(" + str(x) + ")" + "." + str(files)
-            while True:
-                if os.path.exists(path + locations + file_name2):  # file is in location
-                    fileVar = file_name.split(".")
-                    x += 1
-                    file_name2 = str(fileVar[0]) + "(" + str(x) + ")" + "." + str(files)
-                else:
-                    try:
-                        shutil.move(path + file_name, path + locations + file_name2)
-                    except PermissionError as err:
-                        print(err)
+            if filecmp.cmp(path + file_name, path + locations + file_name, shallow=False) is False:
+                fileVar = file_name.split(".")
+                file_name2 = str(fileVar[0]) + "(" + str(x) + ")" + "." + str(files)
+                while True:
+                    if os.path.exists(path + locations + file_name2):  # if file is already there we need to rename it.
+                        if filecmp.cmp(path + file_name, path + locations + file_name2, shallow=False) is False:
+                            fileVar = file_name.split(".")
+                            x += 1
+                            file_name2 = str(fileVar[0]) + "(" + str(x) + ")" + "." + str(files)
+                        else:
+                            break
+                    else:
+                        try:
+                            shutil.move(path + file_name, path + locations + file_name2)
+                        except PermissionError as err:
+                            print(err)
                     break
-        else:
-            try:
-                shutil.move(path + file_name, path + locations + file_name)
-            except PermissionError as err:
-                print(err)
-    elif file_name in folder_name or file_name in folderSubNames or file_name == "desktop.ini":
+            else:
+                try:
+                    shutil.move(path + file_name, path + locations + file_name)
+                except PermissionError as err:
+                    print(err)
+    elif file_name in folderName or file_name in folderSubNames or file_name == "desktop.ini":
         pass
     else:
         print(f"{file_name} Not found")
 
 for root, dirs, files in os.walk(path, topdown=False):
     for name in dirs:
-        if len(os.listdir(os.path.join(root, name))) == 0:  # check whether the directory is empty
+        if len(os.listdir(os.path.join(root, name))) == 0:  # check if directory is empty, if it is than delete it
             print("Deleting", os.path.join(root, name))
-            # print("This is the command to delete for testing deletions :", os.path.join(root, name))
             if os.rmdir(os.path.join(root, name)):
                 print('Successfully deleted', os.path.join(root, name))
